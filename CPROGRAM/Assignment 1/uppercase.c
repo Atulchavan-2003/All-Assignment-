a13815264dade761d01db63d2f3023d171da793c
Atul chavan <atulch20200@gmail.com>
2025-06-30 17:39:23 +0530

    Assignment 01

---
+++ CPROGRAM/Assignment 1/uppercase.c
@@ -0,0 +1,18 @@
+//6. Write a program to check whether a given character is uppercase or lowercase.
+#include<stdio.h>
+void main(){
+	char ch;
+	 	printf("Enter the character \n");
+		scanf("%d",&ch);
+	 	 
+	 	    if(ch >= 'a' && ch <= 'z'){
+	 	 		printf("This is a lowercase");
+			}
+//			else if(cha1 >= 'A' && cha1 <= 'Z'){
+//				printf("This is a lowercase");
+//			}
+//			else{
+//				printf("This is not english word");
+//			}
+	
+}
