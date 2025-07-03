c81a6bc572ff2804ff4dcf02b0a281a4b83fa8d6
Atul chavan <atulch20200@gmail.com>
2025-07-03 13:57:19 +0530

    Update Assignment09.c

--- CPROGRAM/Assignment 3/Assignment09.c
+++ CPROGRAM/Assignment 3/Assignment09.c
@@ -3,12 +3,15 @@ Input: n = 121
 Output: Palindrome*/
 #include <stdio.h>
 void main(){
-	int n = 121,rem;
-	int rev;
+	int n = 121,rem ;
+	int rev = 0, temp = n;
 	while(n > 0){
 		rem = n % 10;
 		rev = rev * 10 + rem;   
 		n = n / 10;
 	}
-	printf("%d",rev);
+	if(temp == rev)
+		printf("pallindrame");
+	else 
+		printf("not pallindrame");
 }
