630bd1b02eb30aeac26701a34ab04313c09126b7
Atul chavan <atulch20200@gmail.com>
2025-07-18 15:58:02 +0530

    Update Assignment10.c

--- CPROGRAM/array_Assingment06/Assignment10.c
+++ CPROGRAM/array_Assingment06/Assignment10.c
@@ -1,19 +1,25 @@
 #include<stdio.h>
-void main(){
-	int arr[5];
+void main() {
+	int arr[]= {5,3,1,8,9,7,2};
 	printf("Enter the array");
-	for(int i=0;i<5;i++){
-		scanf("%d ",&arr[i]);
-	}
-	for(int i=0;i<5;i++){
-		printf("%d ",&arr[i]);
-	}
-	for(int i = 0;i<5;i++){
-		for(int j=1;j<5;j++){
-			if(arr[j] > arr[j+1]){
+	int size = sizeof(arr)/sizeof(arr[0]);
+
+	for(int i=0; i<size-1; i++) {
+		int flag=0;
+		for(int j = 0; j<size-1-i; j++) {
+			int tem = 0;
+			if(arr[j]>arr[j+1]) {
+				flag=1;
+				tem = arr[j];
 				arr[j]=arr[j+1];
+				arr[j+1]=tem;
 			}
-			printf("hello %d",arr[i]);
 		}
+
+		if(flag==0)
+			break;
 	}
+	for(int k =0; k<size; k++)
+		printf("%d ",arr[k]);
+
 }
